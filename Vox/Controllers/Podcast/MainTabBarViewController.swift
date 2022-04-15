e7e234b51cdb373c947094cd3e7ba533ab808319
SkanderThabet <skander.thabet@esprit.tn>
2022-04-15 01:40:43 +0100

    Setting up tab bar for podcast

--- Vox/Controllers/Podcast/MainTabBarViewController.swift
+++ Vox/Controllers/Podcast/MainTabBarViewController.swift
@@ -11,11 +11,18 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        UINavigationBar.appearance().prefersLargeTitles = true
         tabBar.tintColor = .purple
         // Do any additional setup after loading the view.
         
         
+        setupViewControllers()
+    }
+    
+    
+    // MARK: - Setup Functions
+    
+    func setupViewControllers() {
         viewControllers = [
             generateNavigationController(for: ViewController(), title: "Search", image: UIImage(named: "search")!),
             generateNavigationController(for: ViewController(), title: "Favorites", image: UIImage(named: "favorites")!),
@@ -24,10 +31,12 @@ class MainTabBarViewController: UITabBarController {
         ]
     }
     
+    // MARK: - Helper Function
+    
     fileprivate func generateNavigationController(for rootViewController : UIViewController, title : String, image: UIImage) -> UIViewController {
         
         let navController = UINavigationController(rootViewController: rootViewController)
-        navController.navigationBar.prefersLargeTitles = true
+//        navController.navigationBar.prefersLargeTitles = true
         rootViewController.navigationItem.title = title
         navController.tabBarItem.title = title
         navController.tabBarItem.image = image
