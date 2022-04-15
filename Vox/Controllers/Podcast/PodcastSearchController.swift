fff1ce01ab8a2d1de0237eb032fc2b67f3efb2ed
SkanderThabet <skander.thabet@esprit.tn>
2022-04-15 02:52:00 +0100

    cleaned up some lines

--- Vox/Controllers/Podcast/PodcastSearchController.swift
+++ Vox/Controllers/Podcast/PodcastSearchController.swift
@@ -22,18 +22,19 @@ class PodcastSearchController: UITableViewController , UISearchBarDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupSearchBar()
+        setupTableView()
+        
+    }
+    
+    //MARK: - Setup Work
+    
+    fileprivate func setupSearchBar() {
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.delegate = self
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
-        //1. register a cell for our tableview
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
@@ -41,7 +42,13 @@ class PodcastSearchController: UITableViewController , UISearchBarDelegate{
         
         //later implement Alamofire for itunes api search
     }
+    
+    fileprivate func setupTableView() {
+        //1. register a cell for our tableview
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
 
+    
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -59,51 +66,4 @@ class PodcastSearchController: UITableViewController , UISearchBarDelegate{
 
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
