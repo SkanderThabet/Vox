914be64224d7608876e1a34dbf5424c99a9d5485
SkanderThabet <skander.thabet@esprit.tn>
2022-04-15 01:35:13 +0100

    Changed login oAuth button's images & working on podcast

---
+++ Vox/Controllers/Podcast/MainTabBarViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MainTabBarViewController.swift
+//  Vox
+//
+//  Created by Skander Thabet on 15/4/2022.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.tintColor = .purple
+        // Do any additional setup after loading the view.
+        
+        
+        viewControllers = [
+            generateNavigationController(for: ViewController(), title: "Search", image: UIImage(named: "search")!),
+            generateNavigationController(for: ViewController(), title: "Favorites", image: UIImage(named: "favorites")!),
+            generateNavigationController(for: ViewController(), title: "Download", image: UIImage(named: "downloads")!)
+        
+        ]
+    }
+    
+    fileprivate func generateNavigationController(for rootViewController : UIViewController, title : String, image: UIImage) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
