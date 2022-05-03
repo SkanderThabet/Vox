7a7b7a589d5764f2bb95be4b3dd96c642f3bd4a5
SkanderThabet <skander.thabet@esprit.tn>
2022-05-03 02:17:07 +0100

    allow users to swipe open and close our menu based on the velocity of our pan gesture.

--- Vox/Controllers/Main Menu Slider/MainMenuTableViewController.swift
+++ Vox/Controllers/Main Menu Slider/MainMenuTableViewController.swift
@@ -9,25 +9,98 @@ import UIKit
 
 class MainMenuTableViewController: UITableViewController {
 
+    // MARK: - Constants & Variables
+    fileprivate let menuController = MenuController()
+    fileprivate let menuWidth : CGFloat = 300
+    fileprivate var isMenuOpened = false
+    fileprivate let velocityOpenThresholder: CGFloat = 500
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupNavItem()
-    }
-    
-    @objc func handleOpen() {
-        print("Open menu")
-        let menuController = MenuController()
-        menuController.view.frame = CGRect(x: 0, y: 0, width: 300 , height: self.view.frame.height)
+    //MARK: - Menu position
+    fileprivate func initMenuPosition() {
+        menuController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth , height: self.view.frame.height)
         let mainWindow = UIApplication.shared.keyWindow
         mainWindow?.addSubview(menuController.view)
         addChild(menuController)
     }
-    @objc func handleHide() {
-        print("Hide menu")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavItem()
+        initMenuPosition()
+        
+        //Pan Gesture
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        view.addGestureRecognizer(panGesture)
     }
     
+    //MARK: - Perform Actions Function
+    fileprivate func performAnimations(transform : CGAffineTransform) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut) {
+            
+            //final position to animate our menuController object
+            self.menuController.view.transform = transform
+            self.navigationController?.view.transform = transform
+        }
+    }
+    //MARK: - Handle Open
+    @objc func handleOpen() {
+        isMenuOpened = true
+        print("Open menu")
+        performAnimations(transform: CGAffineTransform(translationX: self.menuWidth, y: 0))
+    }
+    
+    @objc func handleHide() {
+        isMenuOpened = false
+        print("Hide menu")
+        performAnimations(transform: .identity)
+    }
+    
+    //MARK: - Handle End
+    fileprivate func handleEnded(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        let velocity = gesture.velocity(in: view)
+        if isMenuOpened {
+            if abs(velocity.x) > velocityOpenThresholder {
+                handleHide()
+                return
+            }
+            if abs(translation.x) < menuWidth / 2 {
+                handleOpen()
+            } else {
+                handleHide()
+            }
+        } else {
+            if velocity.x > velocityOpenThresholder {
+                handleOpen()
+                return
+            }
+            if translation.x < menuWidth / 2 {
+                handleHide()
+            } else {
+                handleOpen()
+            }
+        }
+        
+    }
+    //MARK: - Handle Pan
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        var x: CGFloat = translation.x
+        if gesture.state == .changed {
+            if isMenuOpened {
+                x += menuWidth
+            }
+            
+            x = min(menuWidth, x)
+            x = max(0, x)
+            let transofrm = CGAffineTransform(translationX: x, y: 0)
+            menuController.view.transform = transofrm
+            navigationController?.view.transform = transofrm
+        } else if gesture.state == .ended {
+            handleEnded(gesture : gesture)
+        }
+        
+    }
     fileprivate func setupNavItem() {
         navigationItem.title = "Home"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
